7da930f3fabf9d2338ce488856538ec508a3cd03
rod773 <rodrigoarcadio1@gmail.com>
2026-02-20 11:03:28 +0100

    getUsers service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return [
+      { nombre: "Juan", edad: 25, correo: "juan@example.com" },
+      { nombre: "María", edad: 30, correo: "maria@example.com" },
+      { nombre: "Carlos", edad: 28, correo: "carlos@example.com" },
+      { nombre: "Ana", edad: 32, correo: "ana@example.com" },
+      { nombre: "Pedro", edad: 27, correo: "pedro@example.com" },
+      { nombre: "Laura", edad: 29, correo: "laura@example.com" },
+    ];
   }
+
 }
