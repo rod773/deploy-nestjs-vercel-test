22d6da38bcc6a9073dc709de6793fe2023b2010a
rod773 <rodrigoarcadio1@gmail.com>
2026-02-20 11:12:08 +0100

    enabole cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization,X-Requested-With,Accept-Language",
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
